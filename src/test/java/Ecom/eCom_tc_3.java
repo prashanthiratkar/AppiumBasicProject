40981d52f67b402ac986d9b4c4ff08e992c4d950
Prashant <prashant.hiratkar@gmail.com>
2024-03-13 12:02:08 +0530

    ecom project

--- src/test/java/Ecom/eCom_tc_3.java
+++ src/test/java/Ecom/eCom_tc_3.java
@@ -22,21 +22,11 @@ public class eCom_tc_3 extends BaseTest {
         driver.findElement(By.xpath("//android.widget.EditText[@resource-id=\"com.androidsample.generalstore:id/nameField\"]")).sendKeys("Prashant");
         driver.findElement(By.xpath("//android.widget.RadioButton[@resource-id=\"com.androidsample.generalstore:id/radioMale\"]")).click();
         driver.findElement(By.id("com.androidsample.generalstore:id/btnLetsShop")).click();
-        driver.findElement(AppiumBy.androidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"Jordan 6 Rings\"));"));
-        int product_size= driver.findElements(By.id("com.androidsample.generalstore:id/productName")).size();
-        for(int i=0; i<product_size;i++)
-        {
-            String ProductName=driver.findElements(By.id("com.androidsample.generalstore:id/productName")).get(i).getText();
-            if(ProductName.equalsIgnoreCase("Jordan 6 Rings"))
-            {
-                driver.findElements(By.id("com.androidsample.generalstore:id/productAddCart")).get(i).click();
-            }
-        }
+
         driver.findElement(By.id("com.androidsample.generalstore:id/appbar_btn_cart")).click();
         WebDriverWait wait=new WebDriverWait(driver, Duration.ofSeconds(5));
         wait.until(ExpectedConditions.attributeContains(driver.findElement(By.id("com.androidsample.generalstore:id/toolbar_title")),"text","Cart"));
-        String Prod_name=driver.findElement(By.id("com.androidsample.generalstore:id/productName")).getText();
-        Assert.assertEquals(Prod_name,"Jordan 6 Rings");
+
         Thread.sleep(5000);
     }
 }
